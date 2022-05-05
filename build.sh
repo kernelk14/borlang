2343a31f396b99052a167f76913dc8a89ec0c3df
kernelk <>
2022-05-05 01:49:37 +0000

    Borlang: Inital Commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp main.py ./exec/borlang
+
+./exec/borlang
---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def usage():
+    print("""
+Borlang Programming Language, created by Khyle Isaias at https://github.com/kernelk14
+
+help:
+    borlang [subcommand] <program>
+
+    subcommands:
+          -h    --help            Print this help message
+""")
+
+def run(program):
+    #  TODO: Make a run definition for the code.
+# usage()
