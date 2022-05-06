fdc63b46341e71906106285662bbee9aaf02d86a
kernelk <>
2022-05-06 10:31:44 +0000

    Borlang: Started the Interpretation Stage.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+import os
+import getopt
+import sys
+
+program = '''write "Hello"'''
+
 def usage():
     print("""
 Borlang Programming Language, created by Khyle Isaias at https://github.com/kernelk14
@@ -11,6 +17,13 @@ help:
           -h    --help            Print this help message
 """)
 
+def interpret(program):
+    for p, op in enumerate(program):
+        print(program.split())
+
+
 def run(program):
     #  TODO: Make a run definition for the code.
-# usage()
+    interpret(program)
+usage()
+# run(program)
